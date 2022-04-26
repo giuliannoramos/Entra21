124b1b14c1b41bd71ce617af6879683cdb5ff3b9
Giulianno Ramos <giuleramos@gmail.com>
2022-04-25 22:14:32 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicação inicial/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    using System;
+
+    namespace Aplicação_inicial
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Teste! 1");
+                Console.WriteLine("Teste! 2");
+            }
+        }
+    }
