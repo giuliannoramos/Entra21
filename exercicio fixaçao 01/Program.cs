e6f29ee5c9d9b4107abb3c579172312c32907e02
Giulianno Ramos <giuleramos@gmail.com>
2022-05-03 20:34:16 -0300

    fixação e trabalho

---
+++ exercicio fixaçao 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicio_fixaçao_1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Insira seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("insira sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("seu nome é:" + nome);
+
+            Console.WriteLine("sua idade é:" + idade);
+
+            if (idade < 18)
+            {
+                Console.WriteLine("voce é menor de idade:");
+            }
+
+            else
+            {
+                Console.WriteLine("voce é maior de idade:");
+            }
+
+        }
+    }
+}
