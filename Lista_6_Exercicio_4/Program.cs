4b602093e2c8f186119787643b11491ad3f89209
Giulianno Ramos <giuleramos@gmail.com>
2022-06-10 22:33:46 -0300

    Exercicios adicionais

---
+++ Lista_6_Exercicio_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lista_6_Exercicio_4
+{
+    /*
+     Faça um programa que receba os dados de uma venda:
+    As entidades que devem ter os dados guardados:
+    (USAR LISTAS) List<>
+
+    Adicionar um produto na lista:
+    Produto produto = new Produto();
+    lista.Add(produto);
+
+    Vendedor
+    -Nome;
+    -Código Crachá;
+
+    Cliente
+    -Nome;
+    -CPF;
+    -Endereço;
+
+    Carrinho de compras
+    -Lista de produtos;
+
+    Venda
+    -Lista de produtos;
+    -Valor total na venda (somatório do preço de todos os itens)
+    -Vendedor
+    -Cliente;
+
+    Produto
+    -Descrição do produto
+    -Marca
+    -Preço
+
+    Observações: 
+    Antes de cadastrar uma venda, o vendedor e o cliente precisam estar cadastrados. Para 
+    o carrinho de compras, deve-se ter uma lista com os produtos adicionados e o cliente.
+
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
