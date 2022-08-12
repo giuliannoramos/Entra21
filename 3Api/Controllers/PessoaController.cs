7470888e64bc4cd33be335d8dad43fd2f8db25ee
Giulianno Ramos <giuleramos@gmail.com>
2022-08-11 22:35:20 -0300

    Exercicios adicionais

---
+++ 3Api/Controllers/PessoaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private static readonly List<Pessoa> pessoas = new List<Pessoa>();
+
+        [HttpPost]
+        public IActionResult Salvar(Pessoa pessoa)
+        {
+            pessoas.Add(pessoa);
+            return Ok(pessoa);
+        }
+
+        [HttpPut]
+        public IActionResult Atualizar(List<Pessoa> pessoa, string nome)
+        {
+            Pessoa pEncontrada;            
+            pEncontrada = EncontrarPessoa(pessoa, nome);
+
+            return Ok(pEncontrada);
+        }        
+
+        [HttpGet]
+        public List<Pessoa> BuscarTodos(List<Pessoa> pessoas, string nome)
+        {            
+            var pessoasEncontradas = pessoas.Where(x => x.Nome == (nome)).ToList();
+            return pessoasEncontradas;
+        }
+
+        [HttpDelete]
+        public IActionResult Remover(List<Pessoa> pessoa, string nome)
+        {
+            Pessoa pEncontrada;
+            pEncontrada = EncontrarPessoa(pessoa, nome);
+            
+            return Ok(pEncontrada);
+        }
+
+        static Pessoa EncontrarPessoa(List<Pessoa> pessoas, string nome)
+        {
+            return pessoas.SingleOrDefault(x => x.Nome == nome);
+        }
+
+        //https://localhost:44369/
+    }
+}
